82e0cd177d8fa9a848c0c4574ab9f4dca379f2ee
Vinnycius Amâncio <amancioc3@hotmail.com>
2017-10-10 04:55:52 -0300

    Saving data

--- app/src/main/java/com/example/vinnycius/firebaseappandroid/MainActivity.java
+++ app/src/main/java/com/example/vinnycius/firebaseappandroid/MainActivity.java
@@ -8,15 +8,28 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference firebaseReferencia = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference databaseRefencia = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference usuarioReferencia = databaseRefencia.child("usuarios");
+    private DatabaseReference produtoRefencia = databaseRefencia.child("produtos");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //adicionando nó filho
-        firebaseReferencia.child("pontos").setValue("100");
+        Usuario usuario = new Usuario();
+        usuario.setNome("Vinnycius");
+        usuario.setSobrenome("Amancio");
+        usuario.setIdade(24);
+        usuario.setSexo("Masculino");
 
+        Produto produto = new Produto();
+        produto.setCor("Rosa");
+        produto.setDescricao("iPhone 6s plus");
+        produto.setFabricante("Apple");
+
+        usuarioReferencia.child("003").setValue( usuario);
+
+        produtoRefencia.child("001").setValue(produto);
     }
 }

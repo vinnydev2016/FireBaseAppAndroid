91b8609f5e284ed47395215056f14575a2dfbfa7
Vinnycius Amâncio <amancioc3@hotmail.com>
2017-10-10 03:55:01 -0300

    Final settings

--- app/src/main/java/com/example/vinnycius/firebaseappandroid/MainActivity.java
+++ app/src/main/java/com/example/vinnycius/firebaseappandroid/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.vinnycius.firebaseappandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference firebaseReferencia = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //adicionando nó filho
+        firebaseReferencia.child("pontos").setValue("100");
+
     }
 }
